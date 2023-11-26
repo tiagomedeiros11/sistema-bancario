9b83998dc8c489f03f6fb36bec1613d54bd04c84
Tiago Medeiros da silva lima <tmedeiros@tnsi.com>
2023-11-26 18:16:20 -0300

    Alterada a logica para que as opções do menu sejam modularizadas em funçoes

--- v1.py
+++ v1.py
@@ -15,18 +15,30 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+def deposito(saldo, valor, extrato,/):
+    if valor > 0:        
+        saldo += valor
+        extrato += f"Depósito: R$ {valor:.2f}\n"
+        print(f"Foi realizado um depósito de {valor:.2f} R$ ")
+    else:
+        print("O valor do deposito precisa ser maior que Zero")    
+    
+    return saldo, extrato
+
+def exibir_extrato(saldo, /, *, extrato):
+    print(" Extrato ".center(35,"#",))
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"Seu saldo atual é de R$ {saldo:.2f}")
+    print(" Fim ".center(35,"#"))
 
 while True:
     opcao = input(menu)
 
     if opcao == "1":
-        valor_deposito = int(input("Qual o valor do depósito?"))
-        if valor_deposito > 0:        
-            print(f"Foi realizado um depósito de {valor_deposito:.2f} R$ ")
-            saldo += valor_deposito
-            extrato += f"Depósito: R$ {valor_deposito:.2f}\n"
-        else:
-            print("O valor do deposito precisa ser maior que Zero")    
+        valor = float(input("Qual o valor do depósito?"))
+
+        saldo, extrato = deposito(saldo, valor, extrato)
+            
     elif opcao == "2":
         valor_saque = float(input("Qual o valor do saque?"))
         if numero_saques == LIMITE_SAQUES:
@@ -42,10 +54,7 @@ while True:
             print("Valor indisponivel para saque!")
 
     elif opcao == "3":
-        print(" Extrato ".center(35,"#",))
-        print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"Seu saldo atual é de R$ {saldo:.2f}")
-        print(" Fim ".center(35,"#"))
+        exibir_extrato(saldo, extrato=extrato)
 
     elif opcao == "4":
         print("Obrigado por usar essa bosta!")
